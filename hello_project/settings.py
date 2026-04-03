3a3a3b974fa933a8e6b1c89260c28334e2bb2757
biskvit-studio <muhammadmercedes@gmail.com>
2026-04-03 22:34:34 +0300

    Fix 500 error: use CompressedStaticFilesStorage and add nixpacks.toml for collectstatic

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 # Collected static files go here during build (used by whitenoise in production)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
